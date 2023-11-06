449c97fcc8869ccced44b66a2549cc78513d5e94
aloisorozco <alois.orozco@gmail.com>
2023-11-06 15:43:15 -0500

    added skeleton for scripts + base player movement
    oops i forgot to pull to my branch
    
    TODO:
    -for vertical movement, we need to implement rigidbody in the scene
        (i will write the code but comment it out until that is done)
    -test with best values for speed, health, time allowed to stop, etc.
    -integrate health variable with health bar
    -configure keys in project settings
    -update updateHealth function to make it more interesting/complex
    
    -add sprite to gameobject
    -implement ais for enemies:
        -make it go to player if within a range like in lab
        -make it bounce back and forth between an area
    
    -add health bonuses, timeToStop bonuses, etc. specified in story docs
    -add items/gameobjects in level scene and when those are picked up by player, bonus is given
    
    -much more but for now all i can think of

---
+++ Assets/Scripts/PlayerMechanicsScripts/GreyGoo.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+TODO:
+-add sprite to gameobject
+-implement ais for enemies:
+    -make it go to player if within a range like in lab
+    -make it bounce back and forth between an area
+-theres def more i just cant think of it rn
+*/
+
+public class GreyGoo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
