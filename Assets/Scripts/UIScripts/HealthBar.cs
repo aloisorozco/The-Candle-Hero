a2b230ba389f3636fc71badd9ff8f9edda652084
Toaster545 <95377226+Toaster545@users.noreply.github.com>
2023-11-08 17:15:42 -0500

    Small bug fixes

--- Assets/Scripts/UIScripts/HealthBar.cs
+++ Assets/Scripts/UIScripts/HealthBar.cs
@@ -6,7 +6,6 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
-    [SerializeField] private Image fill;
 
     public void SetHealth(int health, int maxHealth)
     {
