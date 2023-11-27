52b1c684da7e6d7b1d3fe72f4c9247d58bfa655e
Maklapadre <andrekasperkolstad@gmail.com>
2023-11-27 16:43:57 -0500

    Finished Level1 (Platforming Only) & Player Movement

--- Assets/Scripts/PlayerMechanicsScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMechanicsScripts/PlayerMovement.cs
@@ -232,7 +232,7 @@ public class PlayerMovement : MonoBehaviour
     {
         currentLives--;
         if (currentLives == 0) {
-            respawn.GetComponent<RespawnPlayer>().setRespawn(initialRespawnPoint);
+           // respawn.GetComponent<RespawnPlayer>().setRespawn(initialRespawnPoint);
             currentLives = maxLives;
             candles.GetComponent<Candles>().ResetCandles();
         }
@@ -288,12 +288,21 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(movement * Vector2.right);
         }
 
-        if (lastGroundedTime > 0 && Mathf.Abs(move) < movementBuffer)
+        if (isGrounded && Mathf.Abs(move) <= movementBuffer)
         {
             float amount = Mathf.Min(Mathf.Abs(rb.velocity.x), Mathf.Abs(frictionAmount));
             amount *= Mathf.Sign(rb.velocity.x);
             rb.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
 
+        }
+        if (!isGrounded && Mathf.Abs(move) <= movementBuffer)
+        {
+            float targetSpeed = move * runSpeed;
+            //Find the difference between our current speed and the disired speed
+            float speedDif = targetSpeed - rb.velocity.x;
+            float movement = Mathf.Pow(Mathf.Abs(speedDif) * decceleration, velPower) * -transform.localScale.x;
+
+            rb.AddForce(movement * Vector2.right);
         }
     }
 
